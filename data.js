695f04e0324f7f926af4f8e1e0e14a4ca13e8efd
Mohammd Firouzi <mohammad.developer13@gmail.com>
2025-05-03 17:08:48 +0330

    update fils

---
+++ data.js
@@ -0,0 +1,86 @@
+const allLaptops = [
+    // Apple
+    {
+        model: "MacBook Air M1 13\"",
+        brand: "Apple",
+        price: 39000000,
+        image: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/macbook-air-m1-202011",
+        ram: "8GB",
+        cpu: "Apple M1",
+        gpu: "Apple M1 GPU (7-core)"
+    },
+    {
+        model: "MacBook Air M2 13.6\"",
+        brand: "Apple",
+        price: 52000000,
+        image: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/macbook-air-m2-202206",
+        ram: "8GB",
+        cpu: "Apple M2",
+        gpu: "Apple M2 GPU (10-core)"
+    },
+    {
+        model: "MacBook Pro 14\" M3",
+        brand: "Apple",
+        price: 85000000,
+        image: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp14-spaceblack",
+        ram: "16GB",
+        cpu: "Apple M3",
+        gpu: "Apple M3 GPU (10-core)"
+    },
+    // Dell
+    {
+        model: "XPS 13 Plus",
+        brand: "Dell",
+        price: 58000000,
+        image: "https://i.dell.com/sites/imagecontent/products/publishingimages/xps-13-9320-laptop/",
+        ram: "16GB",
+        cpu: "Intel Core i7-1360P",
+        gpu: "Intel Iris Xe Graphics"
+    },
+    {
+        model: "Inspiron 15 3000",
+        brand: "Dell",
+        price: 22000000,
+        image: "https://i.dell.com/sites/imagecontent/products/PublishingImages/inspiron-15-3000",
+        ram: "8GB",
+        cpu: "Intel Core i5-1135G7",
+        gpu: "Intel UHD Graphics"
+    },
+    {
+        model: "Latitude 7430",
+        brand: "Dell",
+        price: 48000000,
+        image: "https://i.dell.com/sites/imagecontent/products/publishingimages/latitude-14-7430",
+        ram: "16GB",
+        cpu: "Intel Core i7-1265U",
+        gpu: "Intel Iris Xe Graphics"
+    },
+    // HP
+    {
+        model: "HP Spectre x360 14",
+        brand: "HP",
+        price: 46000000,
+        image: "https://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c07027814.png",
+        ram: "16GB",
+        cpu: "Intel Core i7-1355U",
+        gpu: "Intel Iris Xe Graphics"
+    },
+    {
+        model: "HP ENVY x360 15",
+        brand: "HP",
+        price: 38000000,
+        image: "https://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c06646922.png",
+        ram: "16GB",
+        cpu: "AMD Ryzen 7 7730U",
+        gpu: "AMD Radeon Graphics"
+    },
+    {
+        model: "HP Pavilion 15",
+        brand: "HP",
+        price: 25000000,
+        image: "https://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c07027900.png",
+        ram: "8GB",
+        cpu: "Intel Core i5-1240P",
+        gpu: "Intel Iris Xe Graphics"
+    }
+];
